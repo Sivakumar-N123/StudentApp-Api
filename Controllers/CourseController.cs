ca16c770f4a6a16a8aa6da40927d65101fb0a2d0
mahima m <mahima.m@qbrainx.com>
2022-09-21 09:31:18 +0530

    data commit

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+
+namespace StudentApp.Controllers
+{
+    [ApiController]
+    [Route("api/course")]
+    public class CourseController : Controller
+    {
+        private readonly StudentAppDBContext _studentAppDBContext;
+        public CourseController(StudentAppDBContext studentAppDBContext)
+        {
+            _studentAppDBContext = studentAppDBContext;
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> GetAllCourses()
+        {
+            var Courses = await _studentAppDBContext.CourseDetailsTable.ToListAsync();
+            return Ok(Courses);
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> AddCourse[FromBody] Course courseRequest)
+        //    {
+        //         courseRequest.Id = Guid.NewGuid(); 
+
+        //         _studentAppDBContext.Course.Addsync(courseRequest);
+             
+        //    }
+    }
+ }
