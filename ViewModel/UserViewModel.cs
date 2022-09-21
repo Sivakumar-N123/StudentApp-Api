12ddd83a0d4f6ce6e1850638f57fdda840196727
1sivakumar <sivakumar.n@qbrainx.com>
2022-09-21 15:32:21 +0530

    usermodel updated

--- ViewModel/UserViewModel.cs
+++ ViewModel/UserViewModel.cs
@@ -7,13 +7,15 @@ namespace StudentApp.ViewModel
         [Key]
         public Guid Id { get; set; }
         [Required]
-        public string User { get; set; }
-        [Required]
         public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
         [Required]
         public string Password { get; set; }
         [Required]
-        public bool IsStudent { get; set; }  
-        public bool IsActive { get; set; }
+        public bool IsStudent { get; set; } = true; 
+        public bool IsActive { get; set; } = true;
     }
 }
