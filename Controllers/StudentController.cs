d81b78177de86e2d371dfadbf516c394e06f1e71
1sivakumar <sivakumar.n@qbrainx.com>
2022-09-21 09:28:32 +0530

    added user controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+
+namespace StudentApp.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class StudentController : Controller
+    {
+        private readonly StudentAppDBContext _studentAppDBContext;
+        public StudentController(StudentAppDBContext studentAppDBContext)
+        {
+            _studentAppDBContext = studentAppDBContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudentlist()
+        {
+            var students = await _studentAppDBContext.UserTable.ToListAsync();
+            return Ok(students);
+        }
+    }
+}
--- ViewModel/UserViewModel.cs
+++ ViewModel/UserViewModel.cs
@@ -7,11 +7,11 @@ namespace StudentApp.ViewModel
         [Key]
         public int Id { get; set; }
         [Required]
-        public int User { get; set; }
+        public string User { get; set; }
         [Required]
         public string UserName { get; set; }
         [Required]
-        public int Password { get; set; }
+        public string Password { get; set; }
         [Required]
         public bool IsStudent { get; set; }  
         public bool IsActive { get; set; }
