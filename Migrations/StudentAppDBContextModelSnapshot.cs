56a833be45cb5761c7d721d7e23da00699908df9
Jaya2Prakash <jayaprakash.r@qbrainx.com>
2022-10-07 14:41:56 +0530

    courseupdate

--- Migrations/StudentAppDBContextModelSnapshot.cs
+++ Migrations/StudentAppDBContextModelSnapshot.cs
@@ -37,9 +37,34 @@ namespace StudentApp.Migrations
                     b.ToTable("CourseDetailsTable");
                 });
 
+            modelBuilder.Entity("StudentApp.ViewModel.SpecCourseViewModel", b =>
+                {
+                    b.Property<Guid>("SpecCourseId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<Guid>("CourseId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("CourseName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<Guid>("SpecificationId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("SpecificationName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("SpecCourseId");
+
+                    b.ToTable("SpecCourseTable");
+                });
+
             modelBuilder.Entity("StudentApp.ViewModel.SpecificationViewModel", b =>
                 {
-                    b.Property<Guid>("ID")
+                    b.Property<Guid>("SpecificationId")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier");
 
@@ -47,9 +72,32 @@ namespace StudentApp.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("ID");
+                    b.HasKey("SpecificationId");
 
                     b.ToTable("SpecificationTable");
+                });
+
+            modelBuilder.Entity("StudentApp.ViewModel.UserCourseDet", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("Course")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Spec")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("username")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("CourseDet");
                 });
 
             modelBuilder.Entity("StudentApp.ViewModel.UserViewModel", b =>
@@ -58,19 +106,25 @@ namespace StudentApp.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier");
 
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<bool>("IsActive")
                         .HasColumnType("bit");
 
                     b.Property<bool>("IsStudent")
                         .HasColumnType("bit");
 
+                    b.Property<bool>("IsVerify")
+                        .HasColumnType("bit");
+
                     b.Property<string>("Password")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("User")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<byte[]>("ProfileImage")
+                        .HasColumnType("varbinary(max)");
 
                     b.Property<string>("UserName")
                         .IsRequired()
---
+++ ViewModel/UserCourseDetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.ViewModel
+{
+    public class UserCourseDetViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string username { get; set; }
+
+        public string Course { get; set; }
+        public string Spec { get; set; }
+
+
+    }
+}
